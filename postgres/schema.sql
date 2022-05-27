fa3651805f41dbbcc4ec31c9dfdde745797931ce
Tinyblargon <tinyblargon@tiny-team.net>
2022-05-27 21:01:19 +0200

    Update Schema

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,18 +1,19 @@
-CREATE TABLE IF NOT EXISTS public.users
+CREATE TABLE IF NOT EXISTS public.Users
 (
-    username text COLLATE pg_catalog."default",
-    binddn text COLLATE pg_catalog."default"
+    UserName text COLLATE pg_catalog."default",
+    BindDn text COLLATE pg_catalog."default"
 );
 
-CREATE TABLE IF NOT EXISTS public.vlans
+CREATE TABLE IF NOT EXISTS public.Vlans
 (
-    vlanname text COLLATE pg_catalog."default",
-    usedbydemo text COLLATE pg_catalog."default"
+    VlanName text COLLATE pg_catalog."default",
+    UsedByDemo text COLLATE pg_catalog."default"
 );
 
-CREATE TABLE IF NOT EXISTS public.runningdemos
+CREATE TABLE IF NOT EXISTS public.RunningDemos
 (
-    demoname text COLLATE pg_catalog."default",
-    username text COLLATE pg_catalog."default",
-    demonumber integer
+    DemoName text COLLATE pg_catalog."default",
+    UserName text COLLATE pg_catalog."default",
+    DemoNumber integer,
+    Runnig boolean
 );
