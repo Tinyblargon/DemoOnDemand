8d30b435eb9dda32332f1899b174994684a9e62f
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2022-06-14 13:32:59 +0200

    Add template create and delete jobs

--- dod/scheduler/job/job.go
+++ dod/scheduler/job/job.go
@@ -8,11 +8,19 @@ import (
 	"github.com/Tinyblargon/DemoOnDemand/dod/helper/programconfig"
 	"github.com/Tinyblargon/DemoOnDemand/dod/helper/session"
 	"github.com/Tinyblargon/DemoOnDemand/dod/helper/taskstatus"
+	"github.com/Tinyblargon/DemoOnDemand/dod/helper/template"
 	"github.com/Tinyblargon/DemoOnDemand/dod/scheduler/backends/memory/demolock"
 )
 
 type Job struct {
-	Demo *Demo
+	Demo     *Demo
+	Template *Template
+}
+
+type Template struct {
+	Config  template.Config
+	Import  bool
+	Destroy bool
 }
 
 type Demo struct {
@@ -26,10 +34,12 @@ type Demo struct {
 }
 
 func (j *Job) Execute(status *taskstatus.Status, demoLock *demolock.DemoLock) {
+	var err error
+	var c *session.Client
 	if j.Demo != nil {
 		ID := j.Demo.UserName + "_" + j.Demo.Template + "_" + strconv.Itoa(int(j.Demo.Number))
 		demoLock.Lock(ID, status)
-		c, err := newSession(status, global.VMwareConfig)
+		c, err = newSession(status, global.VMwareConfig)
 		if err != nil {
 			return
 		}
@@ -51,10 +61,22 @@ func (j *Job) Execute(status *taskstatus.Status, demoLock *demolock.DemoLock) {
 			err = demo.Start(c.VimClient, global.DB, global.VMwareConfig.DataCenter, j.Demo.Template, j.Demo.UserName, j.Demo.Number, status)
 		}
 		demoLock.Unlock(ID)
+	}
+	if j.Template != nil {
+		c, err = newSession(status, global.VMwareConfig)
 		if err != nil {
-			status.AddError(err)
 			return
 		}
+		if j.Template.Import {
+			err = j.Template.Config.Import(c.VimClient, global.VMwareConfig.DataCenter, global.VMwareConfig.Pool, status)
+		}
+		if j.Template.Destroy {
+			err = template.Destroy(c.VimClient, global.VMwareConfig.DataCenter, j.Template.Config.Name, status)
+		}
+	}
+	if err != nil {
+		status.AddError(err)
+		return
 	}
 	status.AddCompleted()
 }
